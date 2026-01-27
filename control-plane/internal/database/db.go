1d674af8bd7a881fd5c3622e73664957a78d327e
Marcel Wierich <marcel@vmhaus.de>
2026-01-27 23:38:25 +0100

    fix: Update DB connection to use correct env vars (DB_USER vs POSTGRES_USER)

--- control-plane/internal/database/db.go
+++ control-plane/internal/database/db.go
@@ -14,9 +14,9 @@ func Init() {
 	dsn := fmt.Sprintf(
 		"host=%s user=%s password=%s dbname=%s port=5432 sslmode=disable TimeZone=UTC",
 		os.Getenv("DB_HOST"),
-		os.Getenv("POSTGRES_USER"),
-		os.Getenv("POSTGRES_PASSWORD"),
-		os.Getenv("POSTGRES_DB"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
 	)
 
 	var err error
